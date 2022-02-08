5896af85eb70d343c2b711099ae36d1d61a64819
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2022-02-08 17:53:35 +0800

    設定 router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ParamComponent} from "./param/param.component";
+import {PathVariableComponent} from "./path-variable/path-variable.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "home", component: HomeComponent},
+  {path: "param", component: ParamComponent},
+  {path: "path", component: PathVariableComponent},
+  {path: "**", component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
