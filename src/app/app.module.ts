3f4608a2416005436b395c6c18edf93fd77b922e
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2022-02-08 17:08:45 +0800

    建立 nav component 建立 home component 建立 param component 建立 pathVariable component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ParamComponent } from './param/param.component';
+import { PathVariableComponent } from './path-variable/path-variable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ParamComponent,
+    PathVariableComponent
   ],
   imports: [
     BrowserModule,
